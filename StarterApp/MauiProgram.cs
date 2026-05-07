73cc7550373c0ea4ada8ca39239377aa03673936
KianChan <40650740@live.napier.ac.uk>
2026-05-07 12:01:36 +0000

    renaming class and updating MauiProgram

--- StarterApp/MauiProgram.cs
+++ StarterApp/MauiProgram.cs
@@ -20,9 +20,18 @@ public static class MauiProgram
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             });
 
-        builder.Services.AddDbContext<AppDbContext>();
-
-        builder.Services.AddSingleton<IAuthenticationService, AuthenticationService>();
+        const bool useSharedApi = false;
+        if (useSharedApi)
+        {
+            var httpClient = new HttpClient { BaseAddress = new Uri("https://set09102-api.b-davison.workers.dev/") };
+            builder.Services.AddSingleton(httpClient);
+            builder.Services.AddSingleton<IAuthenticationService, ApiAuthenticationService>();
+        }
+        else
+        {
+            builder.Services.AddDbContext<AppDbContext>();
+            builder.Services.AddSingleton<IAuthenticationService, LocalAuthenticationService>();
+        }
         builder.Services.AddSingleton<INavigationService, NavigationService>();
 
         builder.Services.AddSingleton<AppShellViewModel>();
