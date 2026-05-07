532b93768db67cbde1ce3fa3bb171ed15cfe9452
KianChan <40650740@live.napier.ac.uk>
2026-05-07 15:03:03 +0000

    adding Category class

---
+++ StarterApp/Models/Category.cs
@@ -0,0 +1,11 @@
+namespace StarterApp.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Slug { get; set; } = string.Empty;
+    public int ItemCount { get; set; }
+
+    public override string ToString() => Name;
+}
