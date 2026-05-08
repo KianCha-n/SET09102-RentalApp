5ee39b63858eea9c42ba0faa438bbdbf72811977
KianChan <40650740@live.napier.ac.uk>
2026-05-08 06:50:15 +0000

    app

--- StarterApp/App.xaml.cs
+++ StarterApp/App.xaml.cs
@@ -16,6 +16,11 @@ public partial class App : Application
 		Routing.RegisterRoute(nameof(Views.UserListPage), typeof(Views.UserListPage));
 		Routing.RegisterRoute(nameof(Views.UserDetailPage), typeof(Views.UserDetailPage));
 		Routing.RegisterRoute(nameof(Views.TempPage), typeof(Views.TempPage));
+		Routing.RegisterRoute(nameof(Views.ItemListPage), typeof(Views.ItemListPage));
+		Routing.RegisterRoute(nameof(Views.ItemDetailPage), typeof(Views.ItemDetailPage));
+		Routing.RegisterRoute(nameof(Views.AddEditItemPage), typeof(Views.AddEditItemPage));
+		Routing.RegisterRoute(nameof(Views.RentalListPage), typeof(Views.RentalListPage));
+		Routing.RegisterRoute(nameof(Views.RentalRequestPage), typeof(Views.RentalRequestPage));
 	}
 
 	protected override Window CreateWindow(IActivationState? activationState)
