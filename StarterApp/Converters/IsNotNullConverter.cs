3bf14b6769a21d8dab6a7fe2efe3c2a8066baafb
KianChan <40650740@live.napier.ac.uk>
2026-05-08 06:49:58 +0000

    IsNotNullConverter

---
+++ StarterApp/Converters/IsNotNullConverter.cs
@@ -0,0 +1,12 @@
+using System.Globalization;
+
+namespace StarterApp.Converters;
+
+public class IsNotNullConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => value != null;
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
