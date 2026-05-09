357676a35c5bc06a827a7d9ec936a1b7cd34ea8f
KianChan <40650740@live.napier.ac.uk>
2026-05-09 17:19:08 +0000

    route for pages

--- StarterApp/AppShell.xaml.cs
+++ StarterApp/AppShell.xaml.cs
@@ -1,12 +1,33 @@
 ﻿using StarterApp.ViewModels;
+using StarterApp.Views;
 
 namespace StarterApp;
+
+
+/// The AppShell class defines the main navigation structure of the app using Shell. 
+/// It registers routes for all the pages in the app and sets up the initial view model for data binding. 
+/// The Shell provides a consistent navigation experience across different platforms and allows for easy page transitions.
 
 public partial class AppShell : Shell
 {
 	public AppShell(AppShellViewModel viewModel)
-	{	
+	{
 		BindingContext = viewModel;
 		InitializeComponent();
+		RegisterRoutes();
+	}
+
+	private static void RegisterRoutes()
+	{
+		Routing.RegisterRoute("MainPage", typeof(MainPage));
+		Routing.RegisterRoute("RegisterPage", typeof(RegisterPage));
+		Routing.RegisterRoute("TempPage", typeof(TempPage));
+		Routing.RegisterRoute("ItemListPage", typeof(ItemListPage));
+		Routing.RegisterRoute("ItemDetailPage", typeof(ItemDetailPage));
+		Routing.RegisterRoute("AddEditItemPage", typeof(AddEditItemPage));
+		Routing.RegisterRoute("RentalListPage", typeof(RentalListPage));
+		Routing.RegisterRoute("RentalRequestPage", typeof(RentalRequestPage));
+		Routing.RegisterRoute("UserListPage", typeof(UserListPage));
+		Routing.RegisterRoute("UserDetailPage", typeof(UserDetailPage));
 	}
 }
