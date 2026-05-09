c4cd35adb4673146bbf12010cfadab0cb89c13ec
KianChan <40650740@live.napier.ac.uk>
2026-05-09 19:07:46 +0000

    remove template page

--- StarterApp/App.xaml.cs
+++ StarterApp/App.xaml.cs
@@ -15,7 +15,6 @@ public partial class App : Application
 		Routing.RegisterRoute(nameof(Views.RegisterPage), typeof(Views.RegisterPage));
 		Routing.RegisterRoute(nameof(Views.UserListPage), typeof(Views.UserListPage));
 		Routing.RegisterRoute(nameof(Views.UserDetailPage), typeof(Views.UserDetailPage));
-		Routing.RegisterRoute(nameof(Views.TempPage), typeof(Views.TempPage));
 		Routing.RegisterRoute(nameof(Views.ItemListPage), typeof(Views.ItemListPage));
 		Routing.RegisterRoute(nameof(Views.ItemDetailPage), typeof(Views.ItemDetailPage));
 		Routing.RegisterRoute(nameof(Views.AddEditItemPage), typeof(Views.AddEditItemPage));
